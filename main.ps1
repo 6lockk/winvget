ce9ec1bc30e6b4b62882f81665926bf5cf0c8788
Anirudh <109010287+6lockk@users.noreply.github.com>
2023-04-11 19:15:17 +0530

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -25,28 +25,39 @@ $downloadUrl = switch ($input) {
         "https://software-download.microsoft.com/download/pr/19042.804.2102183152.co_release_svc_prod1_Windows10_InsiderPreview_Client_x64_en-us.iso"
     }
     4 {
-        "https://software-download.microsoft.com/download/pr/18363.1198.2106120046.rs_x64fre_client_en-us_retail_64e68469.iso"
+        "https://software-download.microsoft.com/download/pr/18363.476.191007-0143.19h2_release_svc_refresh_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_en-us.iso"
     }
     5 {
-        "https://software-download.microsoft.com/download/pr/17134.1.190411-0538.rs4_release_clientbusiness_vol_x64fre_en-us_8a2a278f9589cde83d0ddc7208b5017b6cabbffb.iso"
+        "https://software-download.microsoft.com/download/pr/17134.1.180410-1804.rs4_release_clientbusiness_vol_x64fre_en-us_2cddb565.iso"
     }
 }
 
-$destinationFolder = $null
-while ($destinationFolder -eq $null) {
-    $selectedFolder = (New-Object -ComObject Shell.Application).BrowseForFolder(0, "Select download location", 0, "C:\")
-    if ($selectedFolder -ne $null) {
-        $destinationFolder = $selectedFolder.Self.Path
-    } else {
-        Write-Host "Error: Please select a download location."
+$drive = $null
+while ($drive -eq $null) {
+    $availableDrives = Get-WmiObject Win32_LogicalDisk | Where-Object {$_.DriveType -eq 3} | Select-Object -ExpandProperty DeviceID
+    Write-Host "Please select the destination drive for the download:"
+
+    for ($i = 0; $i -lt $availableDrives.Count; $i++) {
+        Write-Host "$($i+1). $($availableDrives[$i])"
     }
+
+    $driveInput = Read-Host -Prompt "Enter a number (1-$($availableDrives.Count))"
+    if ($driveInput -in 1..$availableDrives.Count) {
+        $drive = $availableDrives[$driveInput - 1]
+    }
+}
+
+$destinationFolder = Join-Path $drive "Windows10Versions"
+
+if (-not (Test-Path $destinationFolder)) {
+    New-Item -ItemType Directory -Path $destinationFolder | Out-Null
 }
 
 $destinationFile = Join-Path $destinationFolder "Windows10Version$($input).iso"
 
 try {
     Invoke-WebRequest $downloadUrl -OutFile $destinationFile -ErrorAction Stop
-    Write-Host "Download complete!"
+    Write-Host "Download complete! File saved at $($destinationFile)"
 } catch {
     if ($_.Exception.Response.StatusCode.Value__ -eq 404) {
         Write-Host "Error: The URL cannot be found."
